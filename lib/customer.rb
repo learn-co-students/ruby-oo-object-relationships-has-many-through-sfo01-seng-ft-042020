76940deff4b7e02c4a67f43327f2f9ccb26c83e5
github username <github email address>
2020-04-23 17:12:37 -0700

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,42 @@
 class Customer
   
+    attr_accessor :name, :age
+
+    @@all = []
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        Meal.all.select { |meal| meal.customer == self }
+    end
+
+    def waiters
+        meals.map { |meal| meal.waiter }
+    end
+
+    # def initialize(waiter, customer, total, tip)
 end
+
+
+# describe "Customer" do
+#     describe "#new_meal" do
+#       it "initializes a meal using the current Customer instance, a provided Waiter instance and a total and tip" do
+#         howard = Customer.new("Howard", 30)
+#         terrance = Waiter.new("Terrance", 1)
+#         howard.new_meal(terrance, 10, 1)
+        
+#         expect(Meal.all.first.waiter).to eq(terrance)
+#         expect(Meal.all.first.customer).to eq(howard)
+#       end
+#     end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,48 @@
 class Waiter
 
+    attr_accessor :name, :years
+
+    @@all = []
+    def initialize(name, years)
+        @name = name
+        @years = years
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total ,tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select { |meal| meal.waiter == self }
+    end
+
+    def best_tipper
+        ele = meals.max {|a, b| a.tip <=> b.tip}
+        ele.customer
+    end
+
+    # array.max_by do |element|
+    #     element.field
+    #   end
+
+    # my_array.max {|a,b| a.attr <=> b.attr }
+
+    # def meals
+    #     Meal.all.select { |meal| meal.customer == self }
+    # end
+
+    # def waiters
+    #     meals.map { |meal| meal.waiter }
+    # end
+
+
+
 end
+
+# initializes current Waiter instance, 
+# Customer total and tip
